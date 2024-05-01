dd522bf26a8f3428ca6e6746ce28c8487ddba5a9
stereobooster <stereobooster@gmail.com>
2024-05-01 19:00:54 +0200

    experiments

--- packages/demo/astro.config.mjs
+++ packages/demo/astro.config.mjs
@@ -4,11 +4,8 @@ import expressiveCode from "./expressiveCode.mjs";
 
 import { getCache } from "@beoe/cache";
 import { rehypeGraphviz } from "@beoe/astro-graphviz/rehype";
-import { rehypePintora } from "@beoe/rehype-pintora";
 import { rehypeMermaid } from "@beoe/rehype-mermaid";
 import { rehypeGnuplot } from "@beoe/rehype-gnuplot";
-// import { rehypeStarryNight } from "@beoe/rehype-starry-night";
-// import { rehypeColorChips } from "@beoe/rehype-color-chips";
 
 const cache = await getCache();
 const className = undefined; // "not-content"
@@ -37,23 +34,8 @@ export default defineConfig({
   markdown: {
     rehypePlugins: [
       [rehypeGraphviz, { class: className }],
-      [
-        // MDX:
-        // without cache it renders 2927ms
-        // with cache it renders 744ms
-        // MD:
-        // without cache it renders 1833ms
-        // with cache it renders 104ms
-        //
-        // Conclusion: don't overuse MDX
-        rehypePintora,
-        { cache, class: className },
-      ],
       [rehypeMermaid, { cache, class: className }],
       [rehypeGnuplot, { cache, class: className }],
-      // either tests fail or demo fails
-      // rehypeColorChips,
-      // rehypeStarryNight,
     ],
   },
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,32 +1,14 @@
-import { defineConfig } from 'vitest/config'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-
-    tsconfigPaths(),
-  ],
+  plugins: [tsconfigPaths()],
   test: {
-    exclude: [
-      '**/vendor/**',
-      '**/node_modules/**',
-    ],
+    exclude: ["**/vendor/**", "**/node_modules/**", "**/experiments/**"],
     coverage: {
-      provider: 'v8',
-      include: [
-        '**/packages/*/src/**/*.ts',
-      ],
-      exclude: [
-        '**/node_modules/**',
-        '**/dist/**',
-        '**/scripts/**',
-
-        // Vendored code
-        '**/vendor/**',
-        '**/stack-element-metadata.ts',
-        '**/src/oniguruma/**',
-        '**/src/assets/**',
-      ],
+      provider: "v8",
+      include: ["**/packages/*/src/**/*.ts"],
+      exclude: ["**/node_modules/**", "**/dist/**", "**/scripts/**"],
     },
     poolOptions: {
       threads: {
@@ -34,4 +16,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
