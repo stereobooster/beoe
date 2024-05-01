431592233f0dc5679013bb623d790cab3aabc715
stereobooster <stereobooster@gmail.com>
2024-05-01 21:03:24 +0200

    for whatever reason I can't fix Astro component

--- packages/rehype-graphviz/src/index.ts
+++ packages/rehype-graphviz/src/index.ts
@@ -3,29 +3,28 @@ import type { Root } from "hast";
 import { processGraphvizSvg } from "./graphviz.js";
 import { rehypeCodeHook, type MapLike } from "@beoe/rehype-code-hook";
 
-// import { Graphviz } from "@hpcc-js/wasm";
-// const graphviz = await Graphviz.load();
-// export const renderGraphviz = ({ code }: { code: string }) =>
-//   processGraphvizSvg(graphviz.dot(code));
-
-import { waitFor } from "@beoe/rehype-code-hook";
-
 export type RenderGraphvizOptions = { code: string; class?: string };
 
+import { Graphviz } from "@hpcc-js/wasm";
+const graphviz = await Graphviz.load();
+export const renderGraphviz = (o: RenderGraphvizOptions) =>
+  processGraphvizSvg(graphviz.dot(o.code), o.class);
+
+// import { waitFor } from "@beoe/rehype-code-hook";
 /**
  * If all graphviz diagrams are cached it would not even load module in memory.
  * If there are diagrams, it would load module and first few renders would be async,
  * but all consequent renders would be sync
  */
-export const renderGraphviz = waitFor(
-  async () => {
-    // @ts-ignore
-    const Graphviz = (await import("@hpcc-js/wasm")).Graphviz;
-    return await Graphviz.load();
-  },
-  (graphviz) => (o: RenderGraphvizOptions) =>
-    processGraphvizSvg(graphviz.dot(o.code), o.class)
-);
+// export const renderGraphviz = waitFor(
+//   async () => {
+//     // @ts-ignore
+//     const Graphviz = (await import("@hpcc-js/wasm")).Graphviz;
+//     return await Graphviz.load();
+//   },
+//   (graphviz) => (o: RenderGraphvizOptions) =>
+//     processGraphvizSvg(graphviz.dot(o.code), o.class)
+// );
 
 export { processGraphvizSvg };
 
