e8f38d3be519a11c77c0076af1402630dd34e4e2
stereobooster <stereobooster@gmail.com>
2024-05-05 15:30:30 +0200

    fix test

--- packages/rehype-mermaid/test/index.test.ts
+++ packages/rehype-mermaid/test/index.test.ts
@@ -3,9 +3,25 @@ import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
-import { expect, it } from "vitest";
+import { afterAll, beforeAll, expect, it, vi } from "vitest";
 
 import rehypeMermaid from "../src";
+
+beforeAll(() => {
+  const m = {
+    random: vi.fn(() => 0.1),
+    trunc: Math.trunc,
+    abs: Math.abs,
+    max: Math.max,
+    floor: Math.floor,
+  };
+
+  vi.stubGlobal("Math", m);
+});
+
+afterAll(() => {
+  vi.unstubAllGlobals();
+});
 
 it("renders diagram", async () => {
   const file = await unified()
