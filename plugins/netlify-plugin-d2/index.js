b3c50b97e91cc3891253b210f3dec55d96689328
stereobooster <stereobooster@gmail.com>
2024-11-20 20:42:45 +0100

    Netlify experiment

--- plugins/netlify-plugin-d2/index.js
+++ plugins/netlify-plugin-d2/index.js
@@ -1,7 +1,3 @@
-import { writeFile } from "fs/promises";
-
 export const onPreBuild = async function ({ utils: { run } }) {
-  const res = await fetch("https://d2lang.com/install.sh");
-  const text = await res.text();
-  await run.command(text);
+  await run.command("brew install d2");
 };
