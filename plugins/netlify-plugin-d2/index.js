04a70f78b86f35f218cad95caf5be90f412627ef
stereobooster <stereobooster@gmail.com>
2024-11-20 20:33:51 +0100

    Netlify experiment

--- plugins/netlify-plugin-d2/index.js
+++ plugins/netlify-plugin-d2/index.js
@@ -3,6 +3,5 @@ import { writeFile } from "fs/promises";
 export const onPreBuild = async function ({ utils: { run } }) {
   const res = await fetch("https://d2lang.com/install.sh");
   const text = await res.text();
-  await writeFile("~/d2i.sh", text);
-  await run.command("~/d2i.sh -s --");
+  await run.command(text);
 };
