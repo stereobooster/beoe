13852f2d9cc376a807a8232bb6fee148936e4e3c
stereobooster <stereobooster@gmail.com>
2024-12-04 16:48:18 +0100

    d2Options docs

--- packages/rehype-d2/src/d2.ts
+++ packages/rehype-d2/src/d2.ts
@@ -52,13 +52,53 @@ export function exec(
 }
 
 export type D2Options = {
+  /**
+   * @default 0
+   * Set the diagram theme ID.
+   */
   theme?: string;
+  /**
+   * @default -1
+   * The theme to use when the viewer's browser is in dark mode.
+   * When left unset --theme is used for both light and dark mode.
+   * Be aware that explicit styles set in D2 code will still be
+   * applied and this may produce unexpected results. We plan on
+   * resolving this by making style maps in D2 light/dark mode
+   * specific. See https://github.com/terrastruct/d2/issues/831.
+   */
   darkTheme?: string;
+  /**
+   * Set the diagram layout engine to the passed string. For a
+   * list of available options, run layout.
+   */
   layout?: string;
+  /**
+   * @default 100
+   * Pixels padded around the rendered diagram.
+   */
   pad?: number;
+  /**
+   * @default -1
+   * Scale the output. E.g., 0.5 to halve the default size.
+   * Default -1 means that SVG's will fit to screen and all others
+   * will use their default render size. Setting to 1 turns off
+   * SVG fitting to screen.
+   */
   scale?: number;
+  /**
+   * @default false
+   * Renders the diagram to look like it was sketched by hand.
+   */
   sketch?: boolean;
+  /**
+   * @default true
+   * Bundle all assets and layers into the output svg.
+   */
   bundle?: boolean;
+  /**
+   * Center the SVG in the containing viewbox, such as your
+   * browser screen.
+   */
   center?: boolean;
 };
 
