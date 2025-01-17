345ac204add8ab7a02e8a8d0c7873b4e478fc61b
stereobooster <stereobooster@gmail.com>
2025-01-17 20:18:06 +0100

    d2 dark theme

--- packages/rehype-d2/src/index.ts
+++ packages/rehype-d2/src/index.ts
@@ -3,10 +3,13 @@ import type { Plugin } from "unified";
 // @ts-expect-error The inferred type of '...' cannot be named without a reference to
 import type { Root } from "hast";
 import { rehypeCodeHookImg } from "@beoe/rehype-code-hook-img";
-import { D2Options, d2New } from "./d2.js";
+import { D2Options, d2 } from "./d2.js";
 
 export type RehypeD2Config = {
-  d2Options?: D2Options;
+  d2Options?: Omit<D2Options, "themeID"> & {
+    theme?: string;
+    darkTheme?: string;
+  };
 };
 
 export const rehypeD2 = rehypeCodeHookImg<RehypeD2Config>({
@@ -14,15 +17,19 @@ export const rehypeD2 = rehypeCodeHookImg<RehypeD2Config>({
   render: async (code: string, opts) => {
     const { darkMode, d2Options, ...rest } = opts;
     const newD2Options = { ...d2Options, ...rest };
-    const svg = await d2New(code, newD2Options);
-    return { svg };
-    // const darkSvg = darkMode
-    //   ? await d2(code, {
-    //       ...d2Options,
-    //       theme: newD2Options?.darkTheme ?? "200",
-    //     })
-    //   : undefined;
-    // return { svg, darkSvg };
+    if (newD2Options.theme !== undefined) {
+      // @ts-ignore
+      newD2Options.themeID = parseFloat(newD2Options.theme);
+    }
+    const svg = await d2(code, newD2Options);
+    const darkSvg = darkMode
+      ? await d2(code, {
+          ...newD2Options,
+          // @ts-ignore
+          themeID: parseFloat(newD2Options?.darkTheme ?? "200"),
+        })
+      : undefined;
+    return { svg, darkSvg };
   },
 });
 
--- plugins/netlify-plugin-d2/index.js
+++
@@ -1,4 +0,0 @@
-export const onPreBuild = async function ({ utils: { run } }) {
-  // HOMEBREW_NO_AUTO_UPDATE=1
-  await run.command("brew install d2");
-};
