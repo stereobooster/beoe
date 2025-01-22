62cbeeba48f92b09bc3633aa38aea88b26cea1ac
stereobooster <stereobooster@gmail.com>
2025-01-22 17:56:30 +0100

    d2: support for import

--- packages/rehype-d2/src/d2.ts
+++ packages/rehype-d2/src/d2.ts
@@ -1,16 +1,40 @@
 // @ts-ignore https://github.com/terrastruct/d2/issues/2286
 import { D2 } from "@terrastruct/d2";
+import { glob, GlobOptions } from "tinyglobby";
+// @ts-expect-error
+import memoizeOne from "async-memoize-one";
+import fs from "node:fs/promises";
 
 export type D2Options = {
   layout?: "dagre" | "elk";
   sketch?: boolean;
   themeID?: number;
   graphFormat?: "graphology" | "dagre";
+  shared?: string | string[] | GlobOptions;
 };
+
+const mGlob = memoizeOne(async (x: string | string[] | GlobOptions) => {
+  // @ts-expect-error
+  const paths = await glob(x);
+  const result: Record<string, string> = Object.create(null);
+  // probably not the best way to do it, but for small number of files should be ok
+  await Promise.all(
+    paths.map((path) =>
+      fs
+        .readFile(path, { encoding: "utf-8" })
+        .then((content) => (result[path] = content))
+    )
+  );
+  return result;
+});
 
 export async function d2(code: string, options: D2Options) {
   const d2Instance = new D2();
-  const result = await d2Instance.compile(code, options);
+
+  const fs: Record<string, string> = options.shared
+    ? { ...(await mGlob(options.shared)), index: code }
+    : { index: code };
+  const result = await d2Instance.compile({ fs }, options);
 
   let data;
   if (options.graphFormat) {
@@ -62,6 +86,6 @@ export async function d2(code: string, options: D2Options) {
     }
   }
 
-  const svg = await d2Instance.render(result.diagram, options) as string;
+  const svg = (await d2Instance.render(result.diagram, options)) as string;
   return { svg, data };
 }
